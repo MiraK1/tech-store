88b703cfd1d3bb52455e27ba1849160b66d9f01f
M7GE <karimgehad@outlook.com>
2020-05-30 15:58:12 +0100

    v1.0.1

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -3,11 +3,11 @@ import Cart from "../components/CartPage";
 import Hero from "../components/Hero";
 import storeBcg from "../images/storeBcg.jpeg";
 
-export default function CartPage() {
+export default function CartPage(props) {
 	return (
 		<>
 			<Hero img={storeBcg} />
-			<Cart />
+			<Cart history={props.history} />
 		</>
 	);
 }
